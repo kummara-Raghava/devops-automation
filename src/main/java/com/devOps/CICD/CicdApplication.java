964cfd26a2734a18f668c854a8d93d3cd412c575
RaghavaKummara <raghava.kummara@vassarlabs.com>
2024-08-06 13:11:27 +0530

    first commit

---
+++ src/main/java/com/devOps/CICD/CicdApplication.java
@@ -0,0 +1,18 @@
+package com.devOps.CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class CicdApplication {
+
+	@GetMapping
+    public String message() {
+		return "Hello World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(CicdApplication.class, args);
+	}
+
+}
